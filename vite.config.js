7b4ad19112f33802792025e022c8bfbdf0346a45
omer <omer88kul@gmail.com>
2026-02-18 13:35:56 +0200

    gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/wrestlingChampionship/',
   plugins: [react()],
+  server: {
+    port: 3000,
+    open: true
+  },
 })
